63987f8cc0bd7e2bc3973f3380e551605c7e723c
Liam O'Hagan <caffeineau@gmail.com>
2018-02-27 21:02:09 +1100

    Fix check for new version

--- Translator/MainWindow.xaml.cs
+++ Translator/MainWindow.xaml.cs
@@ -1187,6 +1187,7 @@ namespace TTSAutomate
             {
                 WebClient wc = new WebClient();
                 wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
                 String responseString = "";
 
 
